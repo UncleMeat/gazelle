f56e870c2d291c11cf5a9adc734d9e14308fca51
mifune <mifune@virginmedia.com>
2012-05-12 15:44:07 +0100

    ripped code from emp to remember search panel status

--- static/functions/browse.js
+++ static/functions/browse.js
@@ -176,3 +176,36 @@ function ToggleEditionRows() {
 	$('#edition_label').toggle();
 	$('#edition_catalogue').toggle();
 }
+  
+function Load_Cookie()  {
+			
+	// the div that will be hidden/shown
+	var panel = jQuery('#search_box');
+	var button = jQuery('#search_button');
+    
+	if(jQuery.cookie('searchPanelState') == undefined) {
+		jQuery.cookie('searchPanelState', 'expanded');
+	}
+	var state = jQuery.cookie('searchPanelState');
+      
+	if(state == 'collapsed') {
+		panel.hide();
+		button.text('Open Search Center');
+	} else {
+		button.text('Close Search Center');
+      }
+	button.click(function(){
+                  if(jQuery.cookie('searchPanelState') == 'expanded') {
+                                          jQuery.cookie('searchPanelState', 'collapsed');
+                                          button.text('Open Search');
+                  } else {
+                                          jQuery.cookie('searchPanelState', 'expanded');
+                                          button.text('Close Search');
+                  }
+                  panel.slideToggle('slow');
+                  return false;
+	});
+}
+ 
+            
+addDOMLoadEvent(Load_Cookie);
