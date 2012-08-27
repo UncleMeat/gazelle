482358c2c9ab27881b4eec527945fbd6082332f1
mifune <mifune@virginmedia.com>
2012-08-27 06:44:59 +0100

    added js zoom and pan to stats chart

--- static/functions/charts.js
+++ static/functions/charts.js
@@ -2,19 +2,21 @@
  *Note: unlike other js files this should not be renamed... privateheader specifically tests for this filename
  * to include a ref to the google js charts api in the head
  */
-function Start_Sitestats(){
+var data;
+var chart;
+var options; 
+var maxrows;
+
+function Start_Google(){
     google.load("visualization", "1", {packages:["corechart"]});
-    google.setOnLoadCallback(drawSiteStats);
+    google.setOnLoadCallback(Start_Sitestats);
 }
 
-function drawSiteStats() {
- 
-    var data = new google.visualization.DataTable(chartdata);
-    /*
-    var data = new google.visualization.arrayToDataTable(chartdata);
-    */
-    var options = {
-            title:title,
+function Start_Sitestats(){
+    data = new google.visualization.DataTable(chartdata);
+    chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+    options = {
+            title:'start',
             height:700,
             chartArea:{left:80,top:20,width:"80%",height:630},
             vAxes:[{gridlines:{color: '#bbb', count: 21}}],
@@ -23,13 +25,59 @@ function drawSiteStats() {
                     {color: 'green', visibleInLegend: true}, 
                     {color: 'red', visibleInLegend: true}],
             tooltip:{ showColorCode: true},
-            hAxis:{slantedText:false,maxTextLines:1,maxAlternation:1}
+            animation:{
+                duration: 2000,
+                easing: 'linear'
+            }, 
+            hAxis:{viewWindow: {min:startrow, max:endrow},slantedText:false,maxTextLines:1,maxAlternation:1}
         }; 
+    maxrows = data.getNumberOfRows()-1;
+    drawChart();
+}
 
-    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+function drawChart() {
+    options.title = data.getValue(options.hAxis.viewWindow.min, 0) + ' to ' + data.getValue(options.hAxis.viewWindow.max, 0);
     chart.draw(data, options);
 }
  
+function zoomout(){
+    options.animation.duration = 1000;
+    var mrange = parseInt(maxrows * 0.15);
+    var shift = mrange;
+    if (options.hAxis.viewWindow.min - mrange < 0) shift = options.hAxis.viewWindow.min;
+    options.hAxis.viewWindow.min -= shift;
+    if (options.hAxis.viewWindow.max + mrange > maxrows) shift = maxrows - options.hAxis.viewWindow.max;
+    else shift = mrange;
+    options.hAxis.viewWindow.max += shift; 
+    drawChart();
+}   
+function zoomin(){
+    options.animation.duration = 1000;
+    var mrange = parseInt(maxrows * 0.15);
+    if ((mrange*3) > (options.hAxis.viewWindow.max-options.hAxis.viewWindow.min)) return;  
+    options.hAxis.viewWindow.min += mrange; 
+    options.hAxis.viewWindow.max -= mrange;
+    drawChart();
+}   
+
+function prev(jump,durationMS) {
+    options.animation.duration = durationMS;
+    var range = parseInt( (options.hAxis.viewWindow.max  - options.hAxis.viewWindow.min) * jump);
+    if (options.hAxis.viewWindow.min - range < 0) range = options.hAxis.viewWindow.min;
+    options.hAxis.viewWindow.min -= range;
+    options.hAxis.viewWindow.max -= range;
+    drawChart();
+}
+
+function next(jump,durationMS) {
+    options.animation.duration = durationMS;
+    var range = parseInt( (options.hAxis.viewWindow.max  - options.hAxis.viewWindow.min) * jump);
+    if (options.hAxis.viewWindow.max + range > maxrows) range = maxrows - options.hAxis.viewWindow.max;
+    options.hAxis.viewWindow.min += range;
+    options.hAxis.viewWindow.max += range;
+    drawChart();
+}
 
 
 
