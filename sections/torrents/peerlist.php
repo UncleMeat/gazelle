39d7b5d2683dc85638af35bea9338de1bbb4b0b5
mifune <mifune@virginmedia.com>
2012-09-01 01:16:47 +0100

    Made info lists in torrents a bit more consistent

--- sections/torrents/peerlist.php
+++ sections/torrents/peerlist.php
@@ -1,13 +1,15 @@
 <?
-if(!isset($_GET['torrentid']) || !is_number($_GET['torrentid'])) { error(404); }
+if (!isset($_GET['torrentid']) || !is_number($_GET['torrentid'])) {
+    error(404);
+}
 $TorrentID = $_GET['torrentid'];
 
-if(!empty($_GET['page']) && is_number($_GET['page'])) {
-	$Page = $_GET['page'];
-	$Limit = (string)(($Page-1)*100) .', 100';
+if (!empty($_GET['page']) && is_number($_GET['page'])) {
+    $Page = $_GET['page'];
+    $Limit = (string) (($Page - 1) * 100) . ', 100';
 } else {
-	$Page = 1;
-	$Limit = 100;
+    $Page = 1;
+    $Limit = 100;
 }
 
 $Result = $DB->query("SELECT SQL_CALC_FOUND_ROWS
@@ -19,7 +21,8 @@ $Result = $DB->query("SELECT SQL_CALC_FOUND_ROWS
 	xu.uploaded,
 	xu.remaining,
 	xu.useragent,
-      IF(xu.remaining=0,1,0) AS IsSeeder
+      IF(xu.remaining=0,1,0) AS IsSeeder,
+	xu.timespent
 	FROM xbt_files_users AS xu
 	LEFT JOIN users_main AS um ON um.ID=xu.uid
 	JOIN torrents AS t ON t.ID=xu.fid
@@ -30,71 +33,61 @@ $Result = $DB->query("SELECT SQL_CALC_FOUND_ROWS
 $DB->query("SELECT FOUND_ROWS()");
 list($NumResults) = $DB->next_record();
 $DB->set_query_id($Result);
-
 ?>
 
-<? if($NumResults>100) { ?>
-<div class="linkbox"><?=js_pages('show_peers', $_GET['torrentid'], $NumResults, $Page)?></div>
-<? } ?>
-<table>
-    <!--
-        <tr class="smallhead">
-            <td colspan="7">Seeders</td>
-        </tr>
-	<tr class="rowa" style="font-weight: bold;">
-		<td>User</td>
-		<td>Active</td>
-
-		<td>Connectable</td>
-
-		<td>Up</td>
-		<td>Down</td>
-		<td>%</td>
-		<td>Ratio</td>
-		<td>Client</td>
-	</tr> -->
-<?
-    $LastIsSeeder = -1;
-while(list($PeerUserID, $Size, $Username, $Active, $Connectable, $Uploaded, $Remaining, $UserAgent, $IsSeeder) = $DB->next_record()) {
-    
-    if ($IsSeeder!=$LastIsSeeder){
-?>
- 
-        <tr class="smallhead">
-            <td colspan="7"><?=($IsSeeder?'Seeders':'Leechers')?></td>
-        </tr>
-	<tr class="rowa" style="font-weight: bold;">
-		<td>User</td>
-		<td>Active</td>
-
-		<td>Connectable</td>
-
-		<td>Up</td>
-		<td>Down</td>
-		<td>%</td>
-		<td>Ratio</td>
-		<td>Client</td>
-	</tr>
-<?
-        $LastIsSeeder = $IsSeeder;
+<? if ($NumResults > 100) { ?>
+    <div class="linkbox"><?= js_pages('show_peers', $_GET['torrentid'], $NumResults, $Page) ?></div>
+    <? } ?>
+<table> 
+    <?
+    if ($NumResults==0){
+            ?> 
+            <tr class="smallhead">
+                <td colspan="8">There are no peers for this torrent</td>
+            </tr> 
+            <?
     }
-?>
-	<tr>
-		<td><?=format_username($PeerUserID, $Username)?></td>
-		<td><?= ($Active) ? '<span style="color:green">Yes</span>' : '<span style="color:red">No</span>' ?></td>
+    $LastIsSeeder = -1;
+    while (list($PeerUserID, $Size, $Username, $Active, $Connectable, $Uploaded, $Remaining, $UserAgent, $IsSeeder, $Timespent) = $DB->next_record()) {
 
-		<td><?= ($Connectable) ? '<span style="color:green">Yes</span>' : '<span style="color:red">No</span>' ?></td>
+        if ($IsSeeder != $LastIsSeeder) {
+            ?>
 
-		<td><?=get_size($Uploaded) ?></td>
-		<td><?=get_size($Size-$Remaining, 2) ?></td>
-		<td><?=number_format(($Size-$Remaining)/$Size*100, 2)?></td>
-		<td><?= number_format( $Uploaded / ($Size-$Remaining) , 3)  ?></td>
-		<td><?=display_str($UserAgent)?></td>
-	</tr>
-<?
-}
-?>
+            <tr class="smallhead">
+                <td colspan="8"><?= ($IsSeeder ? 'Seeders' : 'Leechers') ?></td>
+            </tr>
+            <tr class="rowa" style="font-weight: bold;">
+                <td>User</td>
+                <td>Active</td>
+
+                <td>Connectable</td>
+
+                <td>Up</td>
+                <td>Down</td>
+                <td>%</td>
+                <td>Ratio</td>
+                <td>Client</td>
+            </tr>
+            <?
+            $LastIsSeeder = $IsSeeder;
+        }
+        ?>
+        <tr>
+            <td><?= format_username($PeerUserID, $Username) ?></td>
+            <td><?= ($Active) ? '<span style="color:green">Yes</span>' : '<span style="color:red">No</span>' ?></td>
+
+            <td><?= ($Connectable) ? '<span style="color:green">Yes</span>' : '<span style="color:red">No</span>' ?></td>
+
+            <td><?= get_size($Uploaded) ?></td>
+            <td><?= get_size($Size - $Remaining, 2) ?></td>
+            <td><?= number_format(($Size - $Remaining) / $Size * 100, 2) ?></td>
+            <td><?= number_format($Uploaded / ($Size - $Remaining), 3) ?></td>
+            <td><?= display_str($UserAgent) ?></td>
+        </tr>
+        <?
+    }
+    ?>
 </table>
-<? if($NumResults>100) { ?>
-<div class="linkbox"><?=js_pages('show_peers', $_GET['torrentid'], $NumResults, $Page)?></div>
+<? if ($NumResults > 100) { ?>
+    <div class="linkbox"><?= js_pages('show_peers', $_GET['torrentid'], $NumResults, $Page) ?></div>
 <? } ?>
--- sections/torrents/snatchlist.php
+++ sections/torrents/snatchlist.php
@@ -1,13 +1,15 @@
 <?
-if(!isset($_GET['torrentid']) || !is_number($_GET['torrentid']) || !check_perms('site_view_torrent_snatchlist')) { error(404); }
+if (!isset($_GET['torrentid']) || !is_number($_GET['torrentid']) || !check_perms('site_view_torrent_snatchlist')) {
+    error(404);
+}
 $TorrentID = $_GET['torrentid'];
 
-if(!empty($_GET['page']) && is_number($_GET['page'])) {
-	$Page = $_GET['page'];
-	$Limit = (string)(($Page-1)*100) .', 100';
+if (!empty($_GET['page']) && is_number($_GET['page'])) {
+    $Page = $_GET['page'];
+    $Limit = (string) (($Page - 1) * 100) . ', 100';
 } else {
-	$Page = 1;
-	$Limit = 100;
+    $Page = 1;
+    $Limit = 100;
 }
 
 $Result = $DB->query("SELECT SQL_CALC_FOUND_ROWS
@@ -21,50 +23,60 @@ $Results = $DB->to_array('uid', MYSQLI_ASSOC);
 
 $DB->query("SELECT FOUND_ROWS()");
 list($NumResults) = $DB->next_record();
-
 ?>
 
-<? if($NumResults>100) { ?>
-<div class="linkbox"><?=js_pages('show_snatches', $_GET['torrentid'], $NumResults, $Page)?></div>
-<? } ?>
-
+<? if ($NumResults > 100) { ?>
+    <div class="linkbox"><?= js_pages('show_snatches', $_GET['torrentid'], $NumResults, $Page) ?></div>
+    <? } ?>
 <table>
+    <?
+    if ($NumResults == 0) {
+        ?> 
         <tr class="smallhead">
-                <td colspan="4">Snatches</td>
+            <td colspan="4">There have been no snatches of this torrent</td>
+        </tr> 
+        <?
+    } else {
+        ?>
+        <tr class="smallhead">
+            <td colspan="4">Snatches</td>
         </tr>    
-	<tr class="rowa" style="font-weight: bold;">
-		<td>User</td>
-		<td>Time</td>
-		
-		<td>User</td>
-		<td>Time</td>
-	</tr>
-	<tr>
-<?
-$i = 0;
+        <tr class="rowa" style="font-weight: bold;">
+            <td>User</td>
+            <td>Time</td>
 
-foreach($Results as $ID=>$Data) {
-	list($SnatcherID, $Timestamp) = array_values($Data);
-	
-	$UserInfo = user_info($SnatcherID);
-	
-	$User = format_username($SnatcherID, $UserInfo['Username'], $UserInfo['Donor'], $UserInfo['Warned'], $UserInfo['Enabled'], $UserInfo['PermissionID']);
-	
-	if($i % 2 == 0 && $i>0){
-?> 
-	</tr>
-	<tr>	
-<?
-	}
-?> 
-		<td><?=$User?></td>
-		<td><?=time_diff($Timestamp)?></td>
-<?
-	$i++;
-}
-?>
-	</tr>
+            <td>User</td>
+            <td>Time</td>
+        </tr>
+        <tr>
+            <?
+            $i = 0;
+
+            foreach ($Results as $ID => $Data) {
+                list($SnatcherID, $Timestamp) = array_values($Data);
+
+                $UserInfo = user_info($SnatcherID);
+
+                $User = format_username($SnatcherID, $UserInfo['Username'], $UserInfo['Donor'], $UserInfo['Warned'], $UserInfo['Enabled'], $UserInfo['PermissionID']);
+
+                if ($i % 2 == 0 && $i > 0) {
+                    ?> 
+                </tr>
+                <tr>	
+                    <?
+                }
+                ?> 
+                <td><?= $User ?></td>
+                <td><?= time_diff($Timestamp) ?></td>
+                <?
+                $i++;
+            }
+            ?>
+        </tr>
+        <?
+    }
+    ?>
 </table>
-<? if($NumResults>100) { ?>
-<div class="linkbox"><?=js_pages('show_snatches', $_GET['torrentid'], $NumResults, $Page)?></div>
+<? if ($NumResults > 100) { ?>
+    <div class="linkbox"><?= js_pages('show_snatches', $_GET['torrentid'], $NumResults, $Page) ?></div>
 <? } ?>
