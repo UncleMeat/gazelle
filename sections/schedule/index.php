f4960ce99a61582e681c39a00aba5bf15fa76672
mifune <mifune@virginmedia.com>
2012-09-20 11:04:26 +0100

    disable update geodist in schedule until we nail problem with updating

--- sections/schedule/index.php
+++ sections/schedule/index.php
@@ -450,14 +450,14 @@ if($Day != next_day() || $_GET['runday']){
       list($TorrentCountLastDay) = $DB->next_record();
       $Cache->cache_value('stats_torrent_count_daily', $TorrentCountLastDay, 0); //inf cache
     
-      
+      /*
       $DB->query("INSERT INTO users_geodistribution (Code, Users) 
                        SELECT g.Code, COUNT(u.ID) AS Users 
                          FROM geoip_country AS g JOIN users_main AS u ON INET_ATON(u.IP) BETWEEN g.StartIP AND g.EndIP 
                         WHERE u.Enabled='1' 
                         GROUP BY g.Code 
                      ORDER BY Users DESC");
-
+*/
  
 	//------------- Ratio requirements
 	
