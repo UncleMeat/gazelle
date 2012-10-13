2330dec95f30e8eab975f135cabb9ad777192bdf
mifune <mifune@virginmedia.com>
2012-10-13 23:26:37 +0100

    added users_connectable_status table

--- gazelle.sql
+++ gazelle.sql
@@ -1890,6 +1890,23 @@ CREATE TABLE IF NOT EXISTS `users_collage_subs` (
   KEY `CollageID` (`CollageID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_connectable_status`
+--
+
+CREATE TABLE IF NOT EXISTS `users_connectable_status` (
+ `UserID` int(10) unsigned NOT NULL,
+ `Status` enum('0','1') NOT NULL DEFAULT '1',
+ `Time` int(10) NOT NULL DEFAULT '0',
+ UNIQUE KEY `UserID` (`UserID`),
+ KEY `Status` (`Status`) ,
+ KEY `Time` (`Time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 -- --------------------------------------------------------
 
 --
