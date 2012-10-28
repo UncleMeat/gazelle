d8f94a38bd3ba23667b304ace43489cdbc548fbb
mifune <mifune@virginmedia.com>
2012-10-28 23:23:54 +0000

    bugfix

--- sections/user/takemoderate.php
+++ sections/user/takemoderate.php
@@ -739,7 +739,7 @@ if ($Email){
     if ($Email > 255 ) error ("Email field is too long");
     if (!preg_match("/^".EMAIL_REGEX."$/i", $Email)) error("You did not enter a valid email address:<br/>$Email");
 	$UpdateSet[]="Email='$Email'";
-	$EditSummary[]="email changed from $Cur[email] to $Email";
+	$EditSummary[]="email changed from $Cur[Email] to $Email";
     
 	//This piece of code will update the time of their last email change to the current time *not* the current change.
 	$DB->query("UPDATE users_history_emails SET Time='".sqltime()."' WHERE UserID='$UserID' AND Time='0000-00-00 00:00:00'");
