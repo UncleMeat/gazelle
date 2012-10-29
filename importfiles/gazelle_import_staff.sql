b263f82f43060ca8ea4d5cbe52dcd363c5fe0611
mifune <mifune@virginmedia.com>
2012-10-29 17:23:56 +0000

    forums update

--- importfiles/gazelle_import_staff.sql
+++ importfiles/gazelle_import_staff.sql
@@ -2,5 +2,4 @@
 -- set crozz and asimo to be fls
 UPDATE `users_info` SET `SupportFor`='help and support' WHERE `UserID` IN ( 68969, 88669 );
 -- promote bork
-UPDATE `users_main` SET `PermissionID`='11' WHERE `ID` = 1246;
-UPDATE `users_main` SET `PermissionID`='15' WHERE `ID` = 84499;
+UPDATE `users_main` SET `PermissionID`='11' WHERE `UserID` = 1246;
