6af836ab39592ae97ef33e1998804a5cbe3f427d
mifune <mifune@virginmedia.com>
2012-11-14 21:16:11 +0000

    update

--- classes/script_start.php
+++ classes/script_start.php
@@ -14,10 +14,10 @@
 require 'config.php'; //The config contains all site wide configuration information
 
 
-if (!isset($_GET['in'])) {
-    require SERVER_ROOT . '/redirect_index.php';
-    die();
-} 
+//if (!isset($_GET['in'])) {
+    //require SERVER_ROOT . '/redirect_index.php';
+    //die();
+//} 
 
 //Deal with dumbasses
 if (isset($_REQUEST['info_hash']) && isset($_REQUEST['peer_id'])) {
