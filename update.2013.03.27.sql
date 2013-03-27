4a7977678d465cb144be4a9ced9a99a415f6831e
mifune <mifune@virginmedia.com>
2013-03-27 15:11:28 +0000

    updated sql update data with better names and fix for id

--- update.2013.03.27.sql
+++ update.2013.03.27.sql
@@ -4,9 +4,9 @@ ALTER TABLE `badges_auto` CHANGE `Action` `Action` ENUM( 'NumPosts', 'NumComment
 
 
 INSERT INTO `badges` (`ID`, `Type`, `Display`, `Sort`, `Cost`, `Title`, `Description`, `Image`, `Badge`, `Rank`) VALUES
-(260, 'Single', 1, 80, 0, 'Bounty Hunter', 'awarded for finding 10 dupes', 'bounty-hunter1.png', 'bounty', 1),
-(261, 'Single', 1, 81, 0, 'Bounty Hunter 2', 'awarded for finding 50 dupes', 'bounty-hunter2.png', 'bounty', 2),
-(262, 'Single', 1, 82, 0, 'Bounty Hunter 3', 'awarded for finding 100 dupes', 'bounty-hunter3.png', 'bounty', 3);
+(260, 'Single', 1, 180, 0, 'Bounty Hunter Medal (Cross)', 'Awarded for finding 10 dupes', 'bounty-hunter1.png', 'bounty', 1),
+(261, 'Single', 1, 181, 0, 'Bounty Hunter Medal (Commander)', 'Awarded for finding 50 dupes', 'bounty-hunter2.png', 'bounty', 2),
+(262, 'Single', 1, 182, 0, 'Bounty Hunter Medal (Knight)', 'Awarded for finding 100 dupes', 'bounty-hunter3.png', 'bounty', 3);
 
 
 
