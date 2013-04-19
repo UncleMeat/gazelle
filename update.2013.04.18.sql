2dc96cc39645c1cd27979db5201cd85712b9bc26
mifune <mifune@virginmedia.com>
2013-04-19 02:40:18 +0100

    extended admincomment/bonuslog/seedhistory to mediumtext fields

---
+++ update.2013.04.18.sql
@@ -0,0 +1,4 @@
+ 
+ALTER TABLE `users_info` CHANGE `AdminComment` `AdminComment` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+CHANGE `SeedHistory` `SeedHistory` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+CHANGE `BonusLog` `BonusLog` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ;
