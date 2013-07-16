5d75e1a57ef130051142dc11c90dcaf20b43a889
mifune <mifune@virginmedia.com>
2013-07-16 21:54:01 +0100

    added afterdark css sql update

---
+++ update.2013.07.16.sql
@@ -0,0 +1,6 @@
+ 
+-- --------------------------------------------------------
+
+ INSERT INTO `gazelle`.`stylesheets` ( `ID` , `Name` , `Description` , `Default` ) VALUES ( NULL , 'afterdark', 'b0rk''s dark style css', '0' );
+
+
