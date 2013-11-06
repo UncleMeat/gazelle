4fcc1198bc72147a4eee057d7c4642ebdc564998
UncleMeat <unclemeat1979@live.com>
2013-11-06 12:26:35 +0000

    Fix query to take users off ratio watch

--- sections/schedule/index.php
+++ sections/schedule/index.php
@@ -705,7 +705,7 @@ if($Day != next_day() || $_GET['runday']){
 	
 	// Take users off ratio watch and enable leeching
 	$UserQuery = $DB->query("SELECT m.ID, torrent_pass FROM users_info AS i JOIN users_main AS m ON m.ID=i.UserID
-		WHERE m.Downloaded = 0 OR m.Uploaded/m.Downloaded >= m.RequiredRatio
+		WHERE ( m.Downloaded = 0 OR m.Uploaded/m.Downloaded >= m.RequiredRatio )
 		AND i.RatioWatchEnds!='0000-00-00 00:00:00' 
 		AND m.can_leech='0'
 		AND m.Enabled='1'");
