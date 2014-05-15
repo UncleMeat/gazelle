a60b58aba213c57a085f90ee5f947d1faf37c128
LordRandom <mrr@digitalirc.org>
2014-05-15 21:05:46 +0100

    Update webchat URL

--- redirect_index.php
+++ redirect_index.php
@@ -12,8 +12,8 @@
     <h2> you can find us on irc in the meantime</h2>
     <div class="thin">
         <div class="head">IRC</div>
-        <div class="box pad center">
-                <iframe src="http://shire.digitalirc.org:9090/?prompt=1&uio=MTY9ZmFsc2U67&nick=&channels=empornium" width="98%" height="600"></iframe>
+        <div class="box pad center"> 
+                <iframe src="https://webchat.digitalirc.org/?prompt=1&uio=MTY9ZmFsc2U67&nick=&channels=empornium" width="98%" height="600"></iframe>  
         </div>
     </div>
 </body>
