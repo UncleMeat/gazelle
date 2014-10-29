09b484d90360d8e39d40cda6eb597ad898ac1019
Dax <dax@empornium.me>
2014-10-29 10:53:27 +0000

    typo in requests message.

--- sections/requests/interim.php
+++ sections/requests/interim.php
@@ -35,7 +35,7 @@ show_header(ucwords($Action)." Request");
            if (!check_perms("site_moderate_requests")) { ?>
                 <div class="warning">You will <strong>not</strong> get your bounty back if you delete this request.</div>
 <?php      } else { ?>
-                <div class="warning">This will not return the user's bounty, staff should unfill all votes to delete the request and return bouty automatically.</div>
+                <div class="warning">This will not return the user's bounty, staff should unfill all votes to delete the request and return bounty automatically.</div>
 <?php      }
        } elseif ($Action == 'unfill') { ?>
                 <div class="warning">Unfilling a request without a valid, nontrivial reason will result in a warning.<br/>If in doubt please message the staff and ask for advice first.</div>
@@ -43,7 +43,7 @@ show_header(ucwords($Action)." Request");
                 <input type="hidden" name="voterid" value="<?=$_GET['voterid']?>" />
                 <div class="warning">This will return the user's bounty and, if this is the last vote, it will delete the request.</div>
 <?php      } ?>
-                <strong>Reason:</strong>
+                <strong>The following information is required <br \>Reason:</strong>
                 <textarea name="reason" class="long"/></textarea>
                 <input value="<?=ucwords($Action)?>" type="submit" />
             </form>
