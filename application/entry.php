43a64c1d750167b7f495bcf214460edcfff129f5
UncleMeat <unclemeat1979@live.com>
2014-12-11 21:31:36 +0000

    Register autoloader at entry point

--- application/entry.php
+++ application/entry.php
@@ -1,4 +1,15 @@
 <?php
+namespace gazelle;
+
+function gazelle_autoloader($class_name) {
+    $path = explode('\\', $class_name);
+    if ($path[0] == 'gazelle') {
+        $gazelle_path = array_slice($path, 1);
+        spl_autoload(strtolower(implode('/', $gazelle_path)));
+    }
+}
+
+spl_autoload_register('gazelle\\gazelle_autoloader');
 
 $site_root = dirname(__DIR__);
 set_include_path(get_include_path() . PATH_SEPARATOR . $site_root);
